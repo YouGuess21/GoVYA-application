8fb02d57afd37ba0cd5e4fc4f4cc11e17ac067f8
AshwinM17 <ashwin.mittal3@gmail.com>
2024-04-06 01:02:58 +0530

    Completed Registration Pages

--- Backend/app.py
+++ Backend/app.py
@@ -17,6 +17,7 @@ class DataForm(FlaskForm): #We can create an object of this form
 def home():
     return render_template('Home.html')
 
+
 @app.route('/login',methods=['GET','POST'])
 def login():
     cursor.execute("select * from user")
@@ -34,15 +35,26 @@ def customer():
         return redirect(url_for('unauth'))
 
 
-@app.route('/admin')
+@app.route('/admin',methods=['GET','POST'])
 def Admin():
     welcome = request.args.get('Welcome', default='', type=str)
     userid = request.args.get('userID', default=0, type=int)
     if welcome:
-        return render_template('Admin.html', welcome=welcome, userid=userid)
+        return render_template('Admin.html', username=welcome, userid=userid)
     else:
         return redirect(url_for('unauth'))
-    
+
+@app.route('/admin_funct',methods=['GET','POST'])
+def Admin_fun():
+    if request.method == 'POST':
+        username = request.form.get('user_name', default='', type=str)
+        userid = request.form.get('user_id', default=0, type=int)
+        if username:
+            cursor.execute('SELECT  * from orders')
+            val=cursor.fetchall()
+            return render_template('admin_funct.html', username=username, userid=userid,cur=val)
+    return redirect(url_for('unauth'))
+
 
 @app.route('/Provider')
 def provider():
@@ -64,6 +76,7 @@ def user_selector():
         entered_userid = form.text.data
         form.text.data=""
         entered_pass=form.password.data
+        
         form.password.data=""
         cursor.execute("select * from user")
         value=cursor.fetchall()
@@ -75,7 +88,7 @@ def user_selector():
                     #url_for(fn name,   )
                 if(int(int(row[0])/1000)==2):
                     name=row[2]
-                    return redirect(url_for('Admin',Welcome="Welcome Admin "+name,userID=entered_userid))
+                    return redirect(url_for('Admin',Welcome=name,userID=entered_userid))
                 if(int(int(row[0])/1000)==3):
                     name=row[2]
                     return redirect(url_for('provider',Welcome="Welcome Provider "+name,userID=entered_userid))
@@ -88,13 +101,71 @@ def register():
     return render_template('register.html')
 
 
-@app.route('/cust_reg')
+@app.route('/cust_reg', methods=['GET', 'POST'])
 def cust_register():
-    return render_template('Customer_register.html')
+    if request.method=="POST":
+        name = request.form.get("name")
+        num = request.form.get("pno") #name is the value in name attribute
+        email=request.form.get("email")
+        home=request.form.get("home")
+        passwd=request.form.get("pass")
+        conf=request.form.get("conf")
+        cursor.execute("Select max(user_ID) from user where user_id between 1000 and 1999")
+        id=cursor.fetchone()[0]
+        c_id=int(id)+1
+        cursor.execute("Select email from user")
+        cur=cursor.fetchall()
+        for i in cur:
+            if(i and email==i[0]):
+                return redirect(url_for('cust_register',Success="Email Already Exists,Retry?"))
 
-@app.route('/prov_reg')
+        if(passwd==conf):
+            cursor.execute("insert into user(user_ID,password,name,email) values(%s,%s,%s,%s)",(c_id,passwd,name,email))
+            mydb.commit()
+            cursor.execute("insert into customer(c_id,c_homeaddr,c_phoneNo) values(%s,%s,%s)",(c_id,home,num))
+            mydb.commit()
+            return redirect(url_for('cust_register',Success="Success Account Created:"+str(name) +" User Id:" +str(c_id)))
+        else:
+            return redirect(url_for('cust_register',Success="Passwords Do not match?"))
+
+    success_message = request.args.get('Success', default='', type=str) #when redirected from above(not a post request)
+    return render_template('Customer_register.html',Success=success_message)
+
+@app.route('/prov_reg',methods=['GET', 'POST'])
 def prov_register():
-    return render_template('Provider_register.html')
+    if request.method=="POST":
+        name = request.form.get("name")
+        num = request.form.get("pno") #name is the value in name attribute
+        email=request.form.get("email")
+        office=request.form.get("office")
+        scale=request.form.get("scale")
+        pan = request.form.get("pan")
+        gst = request.form.get("gst")
+        mult = request.form.get("mult")
+        passwd=request.form.get("pass")
+        conf=request.form.get("conf")
+        cursor.execute("Select max(user_ID) from user where user_id between 3000 and 3999")
+        id=cursor.fetchone()[0]
+        p_id=int(id)+1
+        cursor.execute("Select email from user")
+        cur=cursor.fetchall()
+        for i in cur:
+            if(i and email==i[0]):
+                return redirect(url_for('prov_register',Success="Email Already Exists,Retry?"))
+            
+        if(passwd==conf):
+            cursor.execute("insert into user(user_ID,password,name,email) values(%s,%s,%s,%s)",(p_id,passwd,name,email))
+            mydb.commit()
+            cursor.execute("insert into provider(p_id,p_scale,p_officeaddr,p_phoneNo,p_multiplier,p_PAN,p_GST) values(%s,%s,%s,%s,%s,%s,%s)",(p_id,scale,office,num,float(mult),pan,gst))
+            mydb.commit()
+            return redirect(url_for('prov_register',Success="Success Account Created:"+str(name) +" User Id:" +str(p_id)))
+        else:
+            return redirect(url_for('prov_register',Success="Passwords Do not match?"))
+        
+
+
+    success_message = request.args.get('Success', default='', type=str)
+    return render_template('Provider_register.html',Success=success_message)
 
 
 
