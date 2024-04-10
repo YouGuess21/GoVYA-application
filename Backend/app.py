774fd80f37d9bbb8a1f86134217bd1c0a33d7612
AshwinM17 <ashwin.mittal3@gmail.com>
2024-04-10 23:13:24 +0530

    added customer function

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request,redirect,url_for
 import mysql.connector
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import StringField,SubmitField,PasswordField
 
 mydb=mysql.connector.connect(host="localhost",user="root",passwd="Ashwin@319",database="sql_college")
 cursor=mydb.cursor()
@@ -11,6 +11,8 @@ app.config['SECRET_KEY']='nokey'
 class DataForm(FlaskForm): #We can create an object of this form
     text=StringField('Enter User ID:', render_kw={"id": "text"})
     password=StringField('Enter Password:', render_kw={"id": "password"})
+    #Convert in end above is for ease of access while debugging
+    #password=PasswordField('Enter Password:', render_kw={"id": "password"})
     submit=SubmitField('Submit', render_kw={"id": "sub"})
 
 @app.route('/')
@@ -25,22 +27,35 @@ def login():
     form=DataForm()
     return render_template('Login.html',data=value,form=form)
 
-@app.route('/Customer')
+@app.route('/cust_funct',methods=['GET', 'POST'])
+def CustomerFunction():
+    if request.method == 'POST':
+        user_id= request.form.get("user_id", default=0, type=int)
+        todo=request.form.get('todo', default=0,type=int)
+        if(todo==1):
+            cursor.execute("select c_ID,name,email,c_homeaddr,c_phoneNo from user join customer on user_ID=c_id where user_id=%s",(user_id,))
+            userdata=cursor.fetchone() 
+            #used fetchone because only one output would be there
+            #didn't check wether it occured or not as it will definitely exist as we are inside it's login page
+            return render_template('Cust_funct.html',userid=user_id,user_data=userdata,funct=1)
+    return redirect(url_for('unauth'))
+
+@app.route('/Customer',methods=['GET', 'POST'])
 def customer():
-    welcome = request.args.get('Welcome', default='', type=str)
+    uname = request.args.get('Username', default='', type=str)
     userid = request.args.get('userID', default=0, type=int)
-    if welcome:
-        return render_template('Customer.html', Welcome=welcome,userid=userid)
+    if uname:
+        return render_template('Customer.html', username=uname,userid=userid)
     else:
         return redirect(url_for('unauth'))
 
 
 @app.route('/admin',methods=['GET','POST'])
 def Admin():
-    welcome = request.args.get('Welcome', default='', type=str)
+    uname = request.args.get('Username', default='', type=str)
     userid = request.args.get('userID', default=0, type=int)
-    if welcome:
-        return render_template('Admin.html', username=welcome, userid=userid)
+    if uname:
+        return render_template('Admin.html', username=uname, userid=userid)
     else:
         return redirect(url_for('unauth'))
 
@@ -116,11 +131,11 @@ def user_selector():
             if (str(row[0])==entered_userid and str(row[1])==entered_pass):
                 if(int(int(entered_userid)/1000)== 1):
                     name=row[2]
-                    return redirect(url_for('customer',Welcome="Welcome Customer "+name,userID=entered_userid))
+                    return redirect(url_for('customer',Username=name,userID=entered_userid))
                     #url_for(fn name,   )
                 if(int(int(row[0])/1000)==2):
                     name=row[2]
-                    return redirect(url_for('Admin',Welcome=name,userID=entered_userid))
+                    return redirect(url_for('Admin',Username=name,userID=entered_userid))
                 if(int(int(row[0])/1000)==3):
                     name=row[2]
                     return redirect(url_for('provider',Welcome="Welcome Provider "+name,userID=entered_userid))
@@ -131,7 +146,6 @@ def user_selector():
 @app.route('/register')
 def register():
     return render_template('register.html')
-
 
 @app.route('/cust_reg', methods=['GET', 'POST'])
 def cust_register():
@@ -189,6 +203,7 @@ def prov_register():
             cursor.execute("insert into user(user_ID,password,name,email) values(%s,%s,%s,%s)",(p_id,passwd,name,email))
             mydb.commit()
             cursor.execute("insert into provider(p_id,p_scale,p_officeaddr,p_phoneNo,p_multiplier,p_PAN,p_GST,verified) values(%s,%s,%s,%s,%s,%s,%s,%s)",(p_id,scale,office,num,float(mult),pan,gst,False))
+            #Had to use float() as number we get is a string so have to change it to a float
             mydb.commit()
             return redirect(url_for('prov_register',Success="Success Account Created:"+str(name) +" User Id:" +str(p_id)))
         else:
@@ -198,8 +213,6 @@ def prov_register():
 
     success_message = request.args.get('Success', default='', type=str)
     return render_template('Provider_register.html',Success=success_message)
-
-
 
 
 if __name__=="__main__":
